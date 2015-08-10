eea6521fb78c3bd9dcc9940ac271e69f36e622cf
Josh Cox <josh@webhosting.coop>
2015-08-10 14:43:10 -0500

    initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name movieManiaApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the movieManiaApp
+ */
+angular.module('movieManiaApp')
+  .controller('MainCtrl', function () {
+    this.title = 'Thank you for following along';
+  });
